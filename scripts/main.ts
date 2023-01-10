92fb8769b9239145dba4984218580bd41d72d734
KangKang <ws2005ncsd@outlook.com>
2023-01-10 15:34:27 +0800

    add 攻击力 & 毒液附加 enchantment

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,4 +1,14 @@
-import { Block, BlockSignComponent, BlockType, MinecraftBlockTypes, Player, world } from "@minecraft/server";
+import {
+  Block,
+  BlockSignComponent,
+  BlockType,
+  EffectType,
+  EntityInventoryComponent,
+  MinecraftBlockTypes,
+  MinecraftEffectTypes,
+  Player,
+  world,
+} from "@minecraft/server";
 
 function getPlayerByName(playerName: string): Player | undefined {
   let players = Array.from(world.getPlayers());
@@ -14,28 +24,35 @@ function getRandom(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// world.events.blockBreak.subscribe(async (e) => {
-//   e.block.setType(MinecraftBlockTypes.chest);
-// });
+function secToTick(sec: number): number {
+  return sec / 0.05;
+}
 
-world.events.blockPlace.subscribe(async (e) => {
-  let block = e.block;
-  block.setType(
-    MinecraftBlockTypes.getAllBlockTypes()[getRandom(0, MinecraftBlockTypes.getAllBlockTypes().length - 1)]
-  );
+world.events.beforeItemUse.subscribe(async (e) => {
+  e.item.setLore(["§r§l§6攻击力 +7", "§r§l§2毒液附加"]);
 });
 
-world.events.blockBreak.subscribe(async (e) => {
-  // let block = e.block;
-  // block.setType(
-  //   MinecraftBlockTypes.getAllBlockTypes()[getRandom(0, MinecraftBlockTypes.getAllBlockTypes().length - 1)]
-  // );
-  e.player.dimension.runCommandAsync(`scoreboard players add ${e.player.name} breakRank 1`);
-});
+world.events.entityHurt.subscribe(async (e) => {
+  let player = e.damagingEntity as Player;
+  if (player.name != undefined) {
+    let container = (player.getComponent("inventory") as EntityInventoryComponent).container;
+    let lores = container.getItem(player.selectedSlot).getLore();
+    for (const lore of lores) {
+      let name = lore.split(" ")[0];
+      let value = parseInt(lore.split(" ")[1]);
+      switch (name) {
+        case "§r§l§6攻击力":
+          e.hurtEntity.addTag("beAttacked_TempTag");
+          let result = await player.runCommandAsync(
+            `damage @e[tag=beAttacked_TempTag] ${value} entity_attack entity ${player.name}`
+          );
+        case "§r§l§2毒液附加":
+          e.hurtEntity.addEffect(MinecraftEffectTypes.poison, secToTick(10), 1);
+          break;
 
-world.events.blockExplode.subscribe(async (e) => {
-  let block = e.block;
-  block.setType(
-    MinecraftBlockTypes.getAllBlockTypes()[getRandom(0, MinecraftBlockTypes.getAllBlockTypes().length - 1)]
-  );
+        default:
+          break;
+      }
+    }
+  }
 });
